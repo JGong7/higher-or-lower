baf159756aede8087af75c9d5d1c0dc79308ae67
JGong7 <97704595+JGong7@users.noreply.github.com>
2023-12-20 00:48:15 -0800

    attempt homepage

---
+++ App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.gameTitle}>Higher or Lower</Text>
+      <View style={styles.startButton}>
+      <Button title="Start Game" color='white'/>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  gameTitle: {
+    fontWeight: 'bold',
+    margin: 70
+  },
+  startButton: {
+    backgroundColor: '#1DA1F2',
+  }
+});
