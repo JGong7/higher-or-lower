f65e9cf11b792cba608afc1d3270432289db4d7f
JGong7 <97704595+JGong7@users.noreply.github.com>
2023-12-28 19:16:43 -0800

    12/28

--- screens/Game.js
+++ screens/Game.js
@@ -1,31 +1,88 @@
-import React from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Animated, Button, StyleSheet, Text, View } from 'react-native';
 
-export default function Game() {
-    let options = [];
-    fetch("http://localhost:3000/start")
-          .then(response => console.log(response))
-          .then(data => console.log(data))
-          .catch(error => console.log(error));
+export default function Game({navigation}) {
+    const [options, setOptions] = useState([]);
+    const [showButtons, setShowButtons] = useState(true);
+    const [fadeAnim] = useState(new Animated.Value(0));
+    useEffect(() => {
+        fetch(`http://192.168.1.120:3000/start`)
+            .then(response => response.json())
+            .then(data => setOptions(data))  // Set the state to the data returned by the server
+            .catch(error => console.log(error));
+    }, []);
+    async function determineHigher(){
+        setShowButtons(false);
+        if (options[0]?.temperature <= options[1]?.temperature){
+            fetch('http://192.168.1.120:3000/next',{
+                method: 'POST',
+                headers:{
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    city: options[1],
+                }),
+            })
+            .then(response => response.json())
+            .then(data => nextRound(data))
+            .catch(error => console.log(error));
+
+        }else{
+            navigation.navigate('End');
+        }
+    }
+    
+    async function determineLower(){
+        setShowButtons(false);
+        if (options[0]?.temperature >= options[1]?.temperature){
+            fetch('http://192.168.1.120:3000/next',{
+                method: 'POST',
+                headers:{
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    city: options[1],
+                }),
+            })
+            .then(response => response.json())
+            .then(data => nextRound(data))
+            .catch(error => console.log(error));
+        }else{
+            navigation.navigate('End');
+        }
+    }
+
+    function nextRound(city){
+        setOptions([options[1], city]);
+        setShowButtons(true);
+    }
     return (
         <View style={styles.container}>
             <View style={styles.top}>
-                <Text>This city has an average temperature of :</Text>
+                <Text>{options[0]?.city_name} has an average temperature of: {options[0]?.temperature} Celsius</Text>
             </View>
             <View style={styles.bottom}>
-                <Text style={styles.question}>Does this city have higher or lower temperature?</Text>
-                <View style={styles.holdButtons}>
+                <Text style={styles.text}>Does {options[1]?. city_name} have higher or lower temperature?</Text>
+                {showButtons ? (
+                    <View style={styles.holdButtons}>
                     <View style={styles.button}>
-                        <Button title="Higher" color='white' style={styles.button}/>
+                        <Button title='higher' color='white' style={styles.button} onPress={determineHigher}/>
                     </View>
                     <View style={styles.button}>
-                        <Button title="Lower" color='white' style={styles.button}/>
+                        <Button title="Lower" color='white' style={styles.button} onPress={determineLower}/>
                     </View>
                 </View>
+                ) : null}
+                {showResult ? (
+                    <View style={styles.text}> 
+                         <Text>{options[1]?. city_name} has a average temperature of {options[1]?. temperature}</Text>
+                     </View>
+                 ) : null}
             </View>
         </View>
     );
 }
+
 
 const styles = StyleSheet.create({
     container:{
@@ -44,7 +101,7 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center'
     },
-    question: {
+    text: {
         padding: 20
     },
     button: {
--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const port = 3000;
+const lodash = require('lodash');
+const port = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
 const MongoDriver = require('./mongo.js');
 const mongo = new MongoDriver("mongodb+srv://embarrassedboy233:123@cluster0.4q4ed6h.mongodb.net/?retryWrites=true&w=majority");
 let mongoStarted = false;
+
+app.use(bodyParser.json());
 
 app.use(cors());
 
@@ -12,21 +16,40 @@ app.get('/', (req, res) =>{
     res.send("Hello World!");
 })
 
-app.listen(port, () =>{
-    startMongo();
-    console.log("server running at http://localhost:3000");
+app.get('/hi', (req, res) =>{
+    res.send("hi");
 })
 
-app.get('/start', (req, res) => {
-    let all = mongo.read("City");
-    let rand1 = Math.floor(Math.random() * all.length);
-    let rand2 = Math.floor(Math.random() * all.length);
-    while (rand2 == rand1){
-        rand2 = Math.floor(Math.random() * all.length);
+app.listen(port, () =>{
+    startMongo();
+    console.log(`server running at `);
+    console.log(`the port is now https://express-service-dot-noble-mantis-409407.wl.r.appspot.com`);
+})
+
+app.get('/start', async (req, res) => {
+    if (mongoStarted){
+        let all = await mongo.read("City");
+        let rand1 = Math.floor(Math.random() * all.length);
+        let rand2 = Math.floor(Math.random() * all.length);
+        while (rand2 == rand1){
+            rand2 = Math.floor(Math.random() * all.length);
+        }
+        let result = [all[rand1], all[rand2]];
+        res.send(result);
     }
-    let result = [all[rand1], all[rand2]];
-    res.send(result);
   });
+
+app.post('/next', async (req, res) =>{
+    if (mongoStarted){
+        let all = await mongo.read("City");
+        let lastCity = req.body.city;
+        let rand = Math.floor(Math.random() * all.length);
+        while (lodash.isEqual(all[rand], lastCity)){
+            rand = Math.floor(Math.random() * all.length);
+        }
+        res.send(all[rand]);
+    }
+})
 
 
 async function startMongo(){
